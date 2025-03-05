73e5f423e14cb5eb7037a96ea0748e364cce0662
seth <iseth@qq.com>
2025-03-05 17:39:27 +0800

    Initial Commit

---
+++ fxtodo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fxtodo
+//
+//  Created by Seth on 2025/3/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fxtodo/fxtodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  fxtodoApp.swift
+//  fxtodo
+//
+//  Created by Seth on 2025/3/5.
+//
+
+import SwiftUI
+
+@main
+struct fxtodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
